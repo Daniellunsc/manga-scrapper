8af90e67540d2cf5d36a6aaaeea106716719930e
Daniel Luna <daniel.luna@platformbuilders.io>
2020-06-08 18:24:16 -0300

    Removido pasta build

--- build/models/MangaEngine.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MangaEngineService = /** @class */ (function () {
-    function MangaEngineService() {
-    }
-    return MangaEngineService;
-}());
-exports.default = MangaEngineService;
-;
--- build/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MangaBuilder = void 0;
-var Manga_Builder_1 = __importDefault(require("./Manga/Manga.Builder"));
-exports.MangaBuilder = Manga_Builder_1.default;
